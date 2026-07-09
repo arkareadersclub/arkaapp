332b36bebc785b1b1263f0573af9067f4eec027a
abhish3kjain <abhish3kjain@gmail.com>
2026-07-09 13:43:50 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,9 @@
-// Arka App Service Worker
+self.addEventListener('install', (event) => {
+  // Forces the waiting service worker to become the active service worker
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (event) => {
+  // Allows an active service worker to set itself as the controller for all clients within its scope
+  event.waitUntil(clients.claim());
+});
